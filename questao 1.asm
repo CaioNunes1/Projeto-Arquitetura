00da2b947c34e14f060e48a8d07b25a5642c163f
Darllan <darllangomes10@hotmail.com>
2023-11-30 10:59:48 -0300

    questao 1 ajuste

--- questao 1.asm
+++ questao 1.asm
@@ -9,16 +9,16 @@ main:
     la $a0, buffer
     la $a1, str1
     jal strcpy
-    # Agora, buffer cont�m uma c�pia de str1
+    # Agora, buffer contem uma copia de str1
 
     # Teste a funcao memcpy
     la $a0, buffer
     la $a1, str2
     li $a2, 13
     jal memcpy
-    # Agora, os primeiros 13 bytes de buffer cont�m uma c�pia de str2
+    # Agora, os primeiros 13 bytes de buffer contem uma copia de str2
 
-    # Teste a fun��o strcmp
+    # Teste a funcao strcmp
     la $a0, str1
     la $a1, str2
     jal strcmp
@@ -29,7 +29,7 @@ main:
     syscall
 
 
-# Fun��o strcpy
+# Funcao strcpy
 strcpy:
     lbu $t0, 0($a1)       
     sb $t0, 0($a0)        
@@ -40,7 +40,7 @@ strcpy:
 end_strcpy:
     jr $ra               
 
-# Fun��o memcpy
+# Funcao memcpy
 memcpy:
     beqz $a2, end_memcpy  # Se $a2 eh zero, entao termina
     lb $t0, 0($a1)        # Carrega o byte no endereco $a1 para $t0
@@ -50,7 +50,7 @@ memcpy:
     addiu $a2, $a2, -1   
     j memcpy              # Vai para o proximo byte
 end_memcpy:
-    jr $ra                # Retorna para a funcaoo chamadora
+    jr $ra                # Retorna para a funcao chamadora
 
 # Funcao strcmp
 strcmp:
@@ -60,7 +60,7 @@ strcmp:
     beqz $v0, next_char   # Se $v0 eh zero, entao vai para o proximo caractere
     jr $ra                # Retorna para a funcao chamadora
 next_char:
-    beqz $t0, end_strcmp  # Se $t0 � zero (NULL), entao termina
+    beqz $t0, end_strcmp  # Se $t0 eh zero (NULL), entao termina
     addiu $a0, $a0, 1    
     addiu $a1, $a1, 11
     j strcmp              # Vai para o proximo caractere
